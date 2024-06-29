e77373829be480960c9d2a12c699ef1aa4a209e4
Karan <karanmahatocse@gmail.com>
2024-06-29 10:10:42 +0530

    change folder structure

--- twitter-server/src/index.ts
+++ twitter-server/src/index.ts
@@ -1,44 +1,12 @@
-import express from 'express';
-import { ApolloServer } from '@apollo/server';
-import {expressMiddleware } from '@apollo/server/express4';
-import bodyParser from 'body-parser';
-import cors from "cors"
+import { startServer } from "./app";
 
-const typeDefs = `
-    type Todo {
-        id: ID!
-        title: String!
-        complete: Boolean
+async function init(){
+    try {
+        const app = await startServer();
+        app.listen(4000, ()=> console.log("Server started at http://localhost:4000/graphql"));
+    } catch (error) {
+        console.error("Error starting server", error);
     }
-
-    type Query {
-        getTodos: [Todo]
-    }
-`;
-const resolvers = {
-  Query: {
-    getTodos: () => [
-      { id: "1", title: "Learn GraphQL", complete: false },
-      { id: "2", title: "Build an app", complete: true },
-    ],
-  },
-};
-
-async function startServer() {
-  const app = express();
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-
-  app.use(bodyParser.json());
-  app.use(cors());
-
-  await server.start();
-  app.use("/graphql", expressMiddleware(server));
-  app.listen({ port: 4000 }, () =>
-    console.log(`Server ready at http://localhost:4000/graphql`)
-  );
 }
 
-startServer().catch((err) => console.error(err));
+init();
